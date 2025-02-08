e5520ac11342971962daf0f626b1ec6c51ec2dd1
Samuel <samueldiaz180405@gmail.com>
2025-02-07 21:57:40 -0500

    Se completo la interfaz grafica de usuario

--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -1,6 +1,10 @@
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 public class FrmJuego extends JFrame {
@@ -32,7 +36,32 @@ public class FrmJuego extends JFrame {
         lblTitulo2.setBounds(160 + 2 * imagen.getIconWidth(), 10,100,25);
         getContentPane().add(lblTitulo2);
 
+        JLabel lblLanzamientos = new JLabel();
+        lblLanzamientos.setBounds(50 + 2 * imagen.getIconWidth(), 40, 100, 100);
+        lblLanzamientos.setBackground(new Color(0, 0, 0));
+        lblLanzamientos.setForeground(new Color(51, 255, 0));
+        lblLanzamientos.setFont(new Font("Tahoma", 1, 72));
+        lblLanzamientos.setText("0");
+        lblLanzamientos.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblLanzamientos.setOpaque(true);
+        getContentPane().add(lblLanzamientos);
 
+        JLabel lblCenas = new JLabel();
+        lblCenas.setBounds(160 + 2 * imagen.getIconWidth(), 40, 100, 100);
+        lblCenas.setBackground(new Color(0, 0, 0));
+        lblCenas.setForeground(new Color(51, 255, 0));
+        lblCenas.setFont(new Font("Tahoma", 1, 72));
+        lblCenas.setText("0");
+        lblCenas.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblCenas.setOpaque(true);
+        getContentPane().add(lblCenas);
+
+        JButton btnIniciar=new JButton("Iniciar");
+        btnIniciar.setBounds(10, 95+imagen.getIconHeight(), 100, 25);
+        getContentPane().add(btnIniciar);
+        JButton btnLanzar=new JButton("Lanzar");
+        btnLanzar.setBounds(10, 55+imagen.getIconHeight(), 100, 25);
+        getContentPane().add(btnLanzar);
 
     }
 }
