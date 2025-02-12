16fb2e7d2a88acb84606f4283fac72050df51981
Samuel <samueldiaz180405@gmail.com>
2025-02-11 22:16:55 -0500

    Juego de dados terminado

---
+++ src/Dado.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Dado {
+
+    private int numero;
+
+    public void lanzar(Random r) {
+        // generar mumero aleatorio en 1 y 6
+        numero = r.nextInt(6) + 1;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void mostrar(JLabel lbl) {
+        String nombreImagen = "/imagenes/" + String.valueOf(numero) + ".jpg";
+        ImageIcon imagen = new ImageIcon(getClass().getResource(nombreImagen));
+        lbl.setIcon(imagen);
+    }
+
+}
--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -1,5 +1,9 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,34 +13,41 @@ import javax.swing.WindowConstants;
 
 public class FrmJuego extends JFrame {
 
+    private Dado dado1, dado2;
+    private Random r = new Random();
+    private JLabel lblDado1, lblDado2, lblLanzamientos, lblCenas;
+    private int lanzamientos, cenas;
+
     public FrmJuego() {
-        setSize(500,200);
+        setSize(500, 300);
         setTitle("Juguemos a los dados");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(null);
-        
-        JLabel lblDado1= new JLabel();
-        String nombreImagen="imagenes/1.jpg";
-        ImageIcon imagen=new ImageIcon(getClass().getResource(nombreImagen));
+
+        lblDado1 = new JLabel();
+        String nombreImagen = "/imagenes/1.jpg";
+        ImageIcon imagen = new ImageIcon(getClass().getResource(nombreImagen));
         lblDado1.setIcon(imagen);
 
-        lblDado1.setBounds(10,40,imagen.getIconWidth(),imagen.getIconHeight());
+        lblDado1.setBounds(10, 10, imagen.getIconWidth(), imagen.getIconHeight());
         getContentPane().add(lblDado1);
 
-        JLabel lblDado2= new JLabel();
+        lblDado2 = new JLabel();
         lblDado2.setIcon(imagen);
-        lblDado2.setBounds(10+imagen.getIconWidth(),40,imagen.getIconWidth(),imagen.getIconHeight());
+        lblDado2.setBounds(10 + imagen.getIconWidth(), 10, imagen.getIconWidth(), imagen.getIconHeight());
         getContentPane().add(lblDado2);
 
-        JLabel lblTitulo1= new JLabel("Lanzamientos");
-        lblTitulo1.setBounds(50 + 2 * imagen.getIconWidth(), 10,100,25);
+        JLabel lblTitulo1 = new JLabel("Lanzamientos");
+        lblTitulo1.setBounds(50 + 2 * imagen.getIconWidth(), 10, 100, 25);
+        lblTitulo1.setHorizontalAlignment(SwingConstants.CENTER);
         getContentPane().add(lblTitulo1);
 
-        JLabel lblTitulo2= new JLabel("Cenas");
-        lblTitulo2.setBounds(160 + 2 * imagen.getIconWidth(), 10,100,25);
+        JLabel lblTitulo2 = new JLabel("Cenas");
+        lblTitulo2.setBounds(160 + 2 * imagen.getIconWidth(), 10, 100, 25);
+        lblTitulo2.setHorizontalAlignment(SwingConstants.CENTER);
         getContentPane().add(lblTitulo2);
 
-        JLabel lblLanzamientos = new JLabel();
+        lblLanzamientos = new JLabel();
         lblLanzamientos.setBounds(50 + 2 * imagen.getIconWidth(), 40, 100, 100);
         lblLanzamientos.setBackground(new Color(0, 0, 0));
         lblLanzamientos.setForeground(new Color(51, 255, 0));
@@ -46,7 +57,7 @@ public class FrmJuego extends JFrame {
         lblLanzamientos.setOpaque(true);
         getContentPane().add(lblLanzamientos);
 
-        JLabel lblCenas = new JLabel();
+        lblCenas = new JLabel();
         lblCenas.setBounds(160 + 2 * imagen.getIconWidth(), 40, 100, 100);
         lblCenas.setBackground(new Color(0, 0, 0));
         lblCenas.setForeground(new Color(51, 255, 0));
@@ -56,12 +67,54 @@ public class FrmJuego extends JFrame {
         lblCenas.setOpaque(true);
         getContentPane().add(lblCenas);
 
-        JButton btnIniciar=new JButton("Iniciar");
-        btnIniciar.setBounds(10, 95+imagen.getIconHeight(), 100, 25);
+        JButton btnIniciar = new JButton("Iniciar");
+        btnIniciar.setBounds(10, 15 + imagen.getIconHeight(), 100, 25);
         getContentPane().add(btnIniciar);
-        JButton btnLanzar=new JButton("Lanzar");
-        btnLanzar.setBounds(10, 55+imagen.getIconHeight(), 100, 25);
+        JButton btnLanzar = new JButton("Lanzar");
+        btnLanzar.setBounds(10, 45 + imagen.getIconHeight(), 100, 25);
         getContentPane().add(btnLanzar);
 
+        btnIniciar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                iniciar();
+            }
+        });
+
+        btnLanzar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lanzar();
+            }
+        });
+
+        dado1 = new Dado();
+        dado2 = new Dado();
+        lanzamientos = 0;
+        cenas = 0;
     }
+
+    private void iniciar() {
+        lanzamientos = 0;
+        cenas = 0;
+        lblLanzamientos.setText("0");
+        lblCenas.setText("0");
+    }
+
+    private void lanzar() {
+        dado1.lanzar(r);
+        dado2.lanzar(r);
+
+        dado1.mostrar(lblDado1);
+        dado2.mostrar(lblDado2);
+
+        lanzamientos++;
+        lblLanzamientos.setText(String.valueOf(lanzamientos));
+
+        if (dado1.getNumero() + dado2.getNumero() >= 11) {
+            cenas++;
+            lblCenas.setText(String.valueOf(cenas));
+        }
+    }
+
 }
